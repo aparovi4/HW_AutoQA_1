0446751e8fd7779abb6df9ab7f3e1e5854a02611
Neto Aparovich <aparovich.gregory@gmail.com>
2023-03-27 18:54:13 +0300

    initial commit

---
+++ src/main/java/HW_AutoQA_1/CashbackHackService.java
@@ -0,0 +1,9 @@
+package HW_AutoQA_1;
+
+public class CashbackHackService {
+    private final int boundary = 1000;
+
+    public int remain(int amount) {
+        return boundary - amount % boundary;
+    }
+}
